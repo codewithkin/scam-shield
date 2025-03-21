fc369c0be3c4d75279aefcfdbaaec3b6a5d57185
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-21 10:47:34 +0200

    [ADD]: Redirect logic to make sure users are logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 "use client";
+import MainChatPage from "@/components/MainChatPage";
+import { createAuthClient } from "better-auth/react";
+import { redirect } from "next/navigation";
+const { useSession } = createAuthClient() 
 
 function Chat() {
   // Check if the user is logged in
+  const { data: session } = useSession();
 
-  return <div></div>;
+  console.log("Session: ", session);
+
+  // If the user is not logged in....
+  if(!session) {
+    // Redirect to the auth page
+    return redirect("/auth");
+  }
+
+  // Otherwise, continue as normal
+  return (
+    <MainChatPage />
+  )
 }
 
 export default Chat;
