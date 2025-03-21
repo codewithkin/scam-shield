ae77845e534c21ae6b86c261100ee7efea8f7060
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-21 10:49:37 +0200

    [ADD]: Auth page layout file

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Authenticate | ScamShield",
+};
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
