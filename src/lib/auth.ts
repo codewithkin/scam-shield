9532a51e55144a1951c77dc6ab84293bd5ef4dce
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-21 10:47:54 +0200

    [ADD]: NextCookies plugin to ensure cookies are set when users login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
@@ -13,4 +14,8 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     },
   },
+  plugins: [
+    // Make sure this is the last plugin
+    nextCookies()
+  ]
 });
