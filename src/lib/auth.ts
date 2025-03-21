40e6570531e3a9afe715700dae58e79a4da37adf
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-21 11:24:13 +0200

    [ADD]: Shadcn scroll-area component

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,8 +10,8 @@ export const auth = betterAuth({
   }),
   socialProviders: {
     google: {
-      clientId: process.env.GITHUB_CLIENT_ID || "",
-      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     },
   },
   plugins: [
